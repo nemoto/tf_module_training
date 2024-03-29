04a10ef4730c7fbbb538c01298577bd619396f61
soshi nemoto <soshi.nemoto@sharing-innovations.co.jp>
2024-03-29 15:45:21 +0900

    remove unused comment

--- env/.common/variables.tf
+++ env/.common/variables.tf
@@ -7,32 +7,8 @@ locals {
 }
 
 ####################
-# Provider
+# Variables
 ####################
-#variable "proj" {
-#  type        = string
-#  description = "project name"
-#}
-#variable "
-#  type        = string
-#  description = "VPC CIDR"
-#}
-#variable "subnet_public_A_cidr" {
-#  type        = string
-#  description = "public subnet CIDR"
-#}
-#variable "subnet_private_A_cidr" {
-#  type        = string
-#  description = "private subnet CIDR"
-#}
-#variable "subnet_public_C_cidr" {
-#  type        = string
-#  description = "public subnet CIDR"
-#}
-#variable "subnet_private_C_cidr" {
-#  type        = string
-#  description = "private subnet CIDR"
-#}
 variable "ssh_office_cidr" {
   type        = string
   description = "CIDR of of ssh allowed office network for security group rule."
